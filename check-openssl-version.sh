a0df6b2b95f5c47226b881d9277ce0b0ae7c6cd4
Roman Nikitenko <rnikiten@redhat.com>
2024-02-20 19:09:42 +0200

    Clean up
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- check-openssl-version.sh
+++ check-openssl-version.sh
@@ -11,84 +11,84 @@ counter=0
 true >"$REPORT"
 
 if [ $# -gt 0 ]; then
-	IMAGES_LIST_SOURCE="$1"
+  IMAGES_LIST_SOURCE="$1"
 fi
 
 {
-	if [ -f "$IMAGES_LIST_SOURCE" ]; then
-		echo -e "******* Using the following file to read images for testing: $IMAGES_LIST_SOURCE\n******* "
-	else
-		echo -e "!!!!!!! File with images for testing not found: : $IMAGES_LIST_SOURCE.\n!!!!!!! Please provide path to the file with images for testing!"
-		exit 1
-	fi
+  if [ -f "$IMAGES_LIST_SOURCE" ]; then
+    echo -e "******* Using the following file to read images for testing: $IMAGES_LIST_SOURCE\n******* "
+  else
+    echo -e "!!!!!!! File with images for testing not found: : $IMAGES_LIST_SOURCE.\n!!!!!!! Please provide path to the file with images for testing!"
+    exit 1
+  fi
 } >>"$REPORT"
 
 check_openssl_version() {
-	echo ""
-	if command -v openssl &>/dev/null; then
-		echo "+++ OpenSSL command is available"
-	else
-		echo "--- OpenSSL command is not available"
-	fi
+  echo ""
+  if command -v openssl &>/dev/null; then
+    echo "+++ OpenSSL command is available"
+  else
+    echo "--- OpenSSL command is not available"
+  fi
 
-	if command -v rpm &>/dev/null; then
-		echo "+++ RPM command is available"
-	else
-		echo "--- RPM command is not available"
-	fi
+  if command -v rpm &>/dev/null; then
+    echo "+++ RPM command is available"
+  else
+    echo "--- RPM command is not available"
+  fi
 
-	echo ""
-	if command -v openssl &>/dev/null; then
-		echo "=== OpenSSL version is: $(openssl version -v)"
+  echo ""
+  if command -v openssl &>/dev/null; then
+    echo "=== OpenSSL version is: $(openssl version -v)"
 
-		openssl_major_version=$(openssl version -v | cut -d' ' -f2 | cut -d'.' -f1)
-		echo "=== OpenSSL major version is: $openssl_major_version"
-	elif command -v rpm &>/dev/null; then
-		echo "=== OpenSSL version is: $(rpm -qa | grep openssl-libs | cut -d'-' -f3)"
+    openssl_major_version=$(openssl version -v | cut -d' ' -f2 | cut -d'.' -f1)
+    echo "=== OpenSSL major version is: $openssl_major_version"
+  elif command -v rpm &>/dev/null; then
+    echo "=== OpenSSL version is: $(rpm -qa | grep openssl-libs | cut -d'-' -f3)"
 
-		openssl_major_version=$(rpm -qa | grep openssl-libs | cut -d'-' -f3 | cut -d'.' -f1)
-		echo "=== OpenSSL major version is: $openssl_major_version"
-	else
-		echo "ERROR: OpenSSL and rpm commands are not available"
-	fi
+    openssl_major_version=$(rpm -qa | grep openssl-libs | cut -d'-' -f3 | cut -d'.' -f1)
+    echo "=== OpenSSL major version is: $openssl_major_version"
+  else
+    echo "ERROR: OpenSSL and rpm commands are not available"
+  fi
 }
 
 export -f check_openssl_version
 while IFS= read -r image; do
-	if [[ -z "$image" ]] || [[ "$image" == \#* ]]; then
-		continue
-	fi
+  if [[ -z "$image" ]] || [[ "$image" == \#* ]]; then
+    continue
+  fi
 
-	docker pull "$image"
-	((counter++))
+  docker pull "$image"
+  ((counter++))
 
-	{
-		echo "============================== $counter ====================================="
-		echo "Docker image: $image"
-	} >>"$REPORT"
+  {
+    echo "============================== $counter ====================================="
+    echo "Docker image: $image"
+  } >>"$REPORT"
 
-	output="$(docker run --rm --entrypoint=bash "$image" -c "$(declare -f check_openssl_version); check_openssl_version")"
+  output="$(docker run --rm --entrypoint=bash "$image" -c "$(declare -f check_openssl_version); check_openssl_version")"
 
-	if [[ "$output" == *"ERROR"* ]]; then
-		images_with_errors+=("$image")
-	fi
+  if [[ "$output" == *"ERROR"* ]]; then
+    images_with_errors+=("$image")
+  fi
 
-	{
-		echo "$output"
-		echo ""
-	} >>"$REPORT"
+  {
+    echo "$output"
+    echo ""
+  } >>"$REPORT"
 done <"$IMAGES_LIST_SOURCE"
 
 {
-	if [ ${#images_with_errors[@]} -eq 0 ]; then
-		echo "------------------------------- No problems were found for the tested list of images -------------------------------------- "
-		echo "--------------------------------------------------------------------------------------------------------------------------- "
-	else
-		echo "------------------------------- The following images contain problems ------------------------------- "
-		for img in "${images_with_errors[@]}"; do
-			echo "$img"
-		done
-		echo "----------------------------------------------------------------------------------------------------- "
-	fi
+  if [ ${#images_with_errors[@]} -eq 0 ]; then
+    echo "------------------------------- No problems were found for the tested list of images -------------------------------------- "
+    echo "--------------------------------------------------------------------------------------------------------------------------- "
+  else
+    echo "------------------------------- The following images contain problems ------------------------------- "
+    for img in "${images_with_errors[@]}"; do
+      echo "$img"
+    done
+    echo "----------------------------------------------------------------------------------------------------- "
+  fi
 
 } >>"$REPORT"
